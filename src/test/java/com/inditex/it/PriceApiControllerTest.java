c31b5a64dbcb610599b6b5ac0351a6ff7a280f7d
manu <manuvalbu@hotmail.com>
2026-09-11 13:00:04 +0200

    migration to spring boot 4

--- src/test/java/com/inditex/it/PriceApiControllerTest.java
+++ src/test/java/com/inditex/it/PriceApiControllerTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -32,7 +32,7 @@ class PriceApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     FindPriceUseCase priceServiceMock;
 
     @Value("${rest.base-url}")
